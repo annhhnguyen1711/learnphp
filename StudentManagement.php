3418d07acabedbf8b39814a794f653b32dfc7e26
annhhnguyen1711 <143236128+annhhnguyen1711@users.noreply.github.com>
2024-01-01 20:49:52 +0700

    learn

--- StudentManagement.php
+++ StudentManagement.php
@@ -17,5 +17,53 @@ class Student
 
 class StudentManager
 {
+    //Array to store student objects
+    private $student =array();
+
+    //Method to add a new student to the array
+    public function addStudent($id,$name,$address)
+    {
+        $student = new Student($id,$name,$address);
+        $this->students[$id] =$student;
+        echo "Student with ID $id added successfully.\n";
+        echo "<br>";
+    }
+
+    //Method to retrieve a student by ID
+    public function getStudent($id)
+    {
+        if (isset($this->students[$id])){
+            return $this->students[$id];
+        }else{
+            return null;
+        }
+    }
+
+    //Method to update a student's information
+    public function updateStudent($id,$name,$address)
+    {
+        if (isset($this->students[$id])){
+            $this->students[$id]->name = $name;
+            $this->students[$id]->address = $address;
+            echo "Student with ID $id updated successfully.\n";
+            echo "<br>";
+        }else{
+            echo "Student with ID $id not found.\n";
+            echo "<br>";
+        }
+    }
+
+    //Method to delete a studenr by ID
+    public function deleteStudenr($id)
+    {
+        if (isset($this->students[$id])){
+            unset($this->students[$id]);
+            echo "Student with ID $id deleted successfully.\n";
+            echo "<br>";
+        }else{
+            echo "Student with ID $id not found.\n";
+            echo "<br>";
+        }
+    }
 
 }
